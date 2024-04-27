32833ca458aa3ce08514b3364d5a4ad10ca2b01e
wplocienniczak <105588490+wplocienniczak@users.noreply.github.com>
2024-04-27 22:35:29 +0200

    Update main.py
    Dodanie walidacji formularza

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(_name_)
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'  # Ścieżka do bazy SQLite
+app.secret_key = 'secret_key'  # Sekretne klucz używane do flash messages
 db = SQLAlchemy(app)
 
 # Definicja modelu Książki
@@ -10,10 +11,7 @@ class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     author = db.Column(db.String(100), nullable=False)
-    release_date = db.Column(db.String(20), nullable=True)
-    borrowed = db.Column(db.Boolean, default=False)
-    borrowed_by = db.Column(db.String(100), nullable=True)
-
+    release_date = db.Column(db.String(20), nullable=True)  # Dodanie kolumny release_date
 
 
 # Dane do logowania - symulacja
@@ -30,7 +28,8 @@ def login():
     if username in users and users[username] == password:
         return redirect(url_for('search'))
     else:
-        return 'Błędna nazwa użytkownika lub hasło'
+        flash('Błędna nazwa użytkownika lub hasło', 'error')
+        return redirect(url_for('index'))
 
 @app.route('/search')
 def search():
@@ -43,30 +42,19 @@ def add_book():
     author = request.form['author']
     release_date = request.form['release_date']
 
+    if not title or not author or not release_date:
+        flash('Wypełnij wszystkie pola', 'error')
+        return redirect(url_for('search'))
+
     # Tworzenie nowej książki i dodawanie jej do bazy danych
     new_book = Book(title=title, author=author, release_date=release_date)
     db.session.add(new_book)
     db.session.commit()
 
+    flash('Książka dodana pomyślnie', 'success')
     return redirect(url_for('search'))
 
-@app.route('/borrow_book/<int:book_id>', methods=['POST'])
-def borrow_book(book_id):
-    book = Book.query.get(book_id)
-    if book:
-        if not book.borrowed:
-            # Ustawienie flagi wypożyczenia i nazwy użytkownika, który wypożyczył książkę
-            book.borrowed = True
-            book.borrowed_by = request.form['borrower']
-            db.session.commit()
-            return redirect(url_for('search'))
-        else:
-            return 'Książka jest już wypożyczona'
-    else:
-        return 'Nie znaleziono książki'
-
-
-if _name_ == '_main_':
+if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
