5191393d6fafdb78188d187d08d06362152ce69d
wplocienniczak <105588490+wplocienniczak@users.noreply.github.com>
2024-04-27 22:30:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(_name_)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'  # Ścieżka do bazy SQLite
 db = SQLAlchemy(app)
 
@@ -10,7 +10,10 @@ class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     author = db.Column(db.String(100), nullable=False)
-    release_date = db.Column(db.String(20), nullable=True)  # Dodanie kolumny release_date
+    release_date = db.Column(db.String(20), nullable=True)
+    borrowed = db.Column(db.Boolean, default=False)
+    borrowed_by = db.Column(db.String(100), nullable=True)
+
 
 
 # Dane do logowania - symulacja
@@ -47,7 +50,23 @@ def add_book():
 
     return redirect(url_for('search'))
 
-if __name__ == '__main__':
+@app.route('/borrow_book/<int:book_id>', methods=['POST'])
+def borrow_book(book_id):
+    book = Book.query.get(book_id)
+    if book:
+        if not book.borrowed:
+            # Ustawienie flagi wypożyczenia i nazwy użytkownika, który wypożyczył książkę
+            book.borrowed = True
+            book.borrowed_by = request.form['borrower']
+            db.session.commit()
+            return redirect(url_for('search'))
+        else:
+            return 'Książka jest już wypożyczona'
+    else:
+        return 'Nie znaleziono książki'
+
+
+if _name_ == '_main_':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
